d6b251e1590fcf27356e92af38acb3cea488f33c
pixel <taraspixel@gmail.com>
2018-09-25 15:41:54 +0300

    react-online-3-lesson-3

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -6,9 +6,20 @@ import Styles from './styles.m.css';
 
 export default class StatusBar extends Component {
     render () {
+        const {
+            currentUserFirstName,
+            currentUserLastName,
+            avatar,
+        } = this.props;
+
         return (
             <section className = { Styles.statusBar }>
-                <p>status</p>
+                <button>
+                    <img src = { avatar } />
+                    <span>{currentUserFirstName}</span>
+                    &nbsp;
+                    <span>{currentUserLastName}</span>
+                </button>
             </section>
         );
     }
