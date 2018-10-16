99ced5518e83a0312096f946d9539b1c25d757f6
pixel <taraspixel@gmail.com>
2018-10-16 11:43:22 +0300

    react-online-3-lesson-10 practice

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,6 +1,10 @@
 // Core
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import {
+    Transition,
+    CSSTransition,
+    TransitionGroup
+} from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 // Components
@@ -189,14 +193,24 @@ export default class Feed extends Component {
 
         const postsJSX = posts.map((post) => {
             return (
-                <Catcher key = { post.id }>
-                    <Post
-                        key = { post.id }
-                        { ...post }
-                        _likePost = { this._likePost }
-                        _removePost = { this._removePost }
-                    />
-                </Catcher>
+                <CSSTransition
+                    classNames = { {
+                        enter:       Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                    } }
+                    key = { post.id }
+                    timeout = { {
+                        enter: 500,
+                        exit:  400,
+                    } }>
+                    <Catcher>
+                        <Post
+                            { ...post }
+                            _likePost = { this._likePost }
+                            _removePost = { this._removePost }
+                        />
+                    </Catcher>
+                </CSSTransition>
             );
         });
 
@@ -219,7 +233,10 @@ export default class Feed extends Component {
                     onEntered = { this._animatePostmanEntered }>
                     <Postman />
                 </Transition>
-                {postsJSX}
+                <TransitionGroup>
+                    {postsJSX}
+                </TransitionGroup>
+
             </section>
         );
     }
